9f63ff6001803b0acb0b911e5d783a030d580339
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2024-06-06 18:13:12 +0900

    add Page Router

---
+++ wallet-app/src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { AppProps } from "next/app";
+import { WagmiProvider } from "wagmi";
+import { config } from "../wagmi";
+
+const queryClient = new QueryClient();
+
+export default function App({ Component, pageProps }: AppProps) {
+    return (
+        <WagmiProvider config={config}>
+            <QueryClientProvider client={queryClient}>
+                <Component {...pageProps} />
+            </QueryClientProvider>
+        </WagmiProvider>
+    );
+}
---
+++ wallet-app/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function CatPage() {
+    return (
+        <>
+            <Head>
+                <title>wallet-app</title>
+            </Head>
+            <main>
+                <h1>wallet-app</h1>
+            </main>
+        </>
+    );
+}
