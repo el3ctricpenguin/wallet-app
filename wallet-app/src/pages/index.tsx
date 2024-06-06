963521ec2d8c1c609eba931649d80f955763577d
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2024-06-07 01:26:35 +0900

    add connect wallet

--- wallet-app/src/pages/index.tsx
+++ wallet-app/src/pages/index.tsx
@@ -1,8 +1,42 @@
 import Head from "next/head";
-import { useAccount, useBalance, useConnect, useDisconnect, useReadContract } from "wagmi";
+import { Config, Connector, useAccount, useBalance, useConnect, useDisconnect, useReadContract } from "wagmi";
 import { Decimal } from "decimal.js";
 import { erc20Abi, formatUnits } from "viem";
 import { SEPOLIA_USDT } from "@/constants";
+
+function Account({
+    isConnected,
+    connectors,
+    address,
+    connect,
+    disconnect,
+}: {
+    isConnected: boolean;
+    connectors: readonly Connector[];
+    address: `0x${string}` | undefined;
+    connect: any;
+    disconnect: any;
+}) {
+    if (isConnected) {
+        return (
+            <>
+                {address} (
+                <a href="#" onClick={disconnect}>
+                    Disconnect
+                </a>
+                )
+            </>
+        );
+    } else {
+        return (
+            <>
+                <a href="#" onClick={() => connect({ connector: connectors[0] })}>
+                    Connect wallet
+                </a>
+            </>
+        );
+    }
+}
 
 function Balance({ address, tokenAddress = undefined }: { address: `0x${string}` | undefined; tokenAddress: `0x${string}` | undefined }) {
     if (tokenAddress) {
@@ -25,7 +59,7 @@ function Balance({ address, tokenAddress = undefined }: { address: `0x${string}`
     } else {
         const { data, isError, isLoading } = useBalance({ address: address });
         const tokenSymbol = data?.symbol;
-        const tokenName = tokenSymbol == "ETH" ? "Ethereum" : "Mantle";
+        const tokenName = tokenSymbol == "MNT" ? "Mantle" : "Ethereum";
         const tokenBalance = data ? formatUnits(data.value, data.decimals) : "-";
         console.log(data);
         return (
@@ -47,6 +81,9 @@ export default function WalletApp() {
             </Head>
             <main>
                 <h1>wallet-app</h1>
+                <h3>Account:</h3>
+                <Account isConnected={isConnected} connectors={connectors} address={address} connect={connect} disconnect={disconnect} />
+                <h3>Balances:</h3>
                 <Balance address={address} tokenAddress={undefined} />
                 <Balance address={address} tokenAddress={SEPOLIA_USDT} />
             </main>
