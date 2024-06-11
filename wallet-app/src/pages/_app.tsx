6a0f0d3ce3843063c95624185ccdb3e66238e021
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2024-06-11 16:47:35 +0900

    import Chakra UI

--- wallet-app/src/pages/_app.tsx
+++ wallet-app/src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+
 import { WagmiProvider } from "wagmi";
 import { config } from "../wagmi";
 
@@ -9,7 +11,9 @@ export default function App({ Component, pageProps }: AppProps) {
     return (
         <WagmiProvider config={config}>
             <QueryClientProvider client={queryClient}>
-                <Component {...pageProps} />
+                <ChakraProvider>
+                    <Component {...pageProps} />
+                </ChakraProvider>
             </QueryClientProvider>
         </WagmiProvider>
     );
