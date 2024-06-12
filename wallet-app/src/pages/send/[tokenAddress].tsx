0825bf28392d14d91984ad8fdaf13d176e234fc5
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2024-06-12 23:31:00 +0900

    add send function for ETH

--- wallet-app/src/pages/send/[tokenAddress].tsx
+++ wallet-app/src/pages/send/[tokenAddress].tsx
@@ -1,11 +1,11 @@
 import { useRouter } from "next/router";
 import NextLink from "next/link";
 import { type ReactElement, useState } from "react";
-import { useAccount, useBalance, useConnect, useDisconnect, useReadContract } from "wagmi";
+import { useAccount, useBalance, useConnect, useDisconnect, useReadContract, useSendTransaction } from "wagmi";
 import { Alert, AlertIcon, Button, Heading, Input, InputGroup, InputRightAddon, Link, Text } from "@chakra-ui/react";
 import { ChevronLeftIcon } from "@chakra-ui/icons";
 
-import { erc20Abi, formatUnits, getAddress, isAddress } from "viem";
+import { erc20Abi, formatUnits, getAddress, isAddress, parseEther } from "viem";
 
 import Account from "../../components/Account";
 import Balance from "../../components/Balance";
@@ -15,7 +15,7 @@ import WalletWrapper from "@/components/WalletWrapper";
 
 function BackToTopLink(): ReactElement {
     return (
-        <Link as={NextLink} href="/" textDecoration="underline" _hover={{ fontStyle: "italic" }}>
+        <Link as={NextLink} href="/" textDecoration="underline" mt={4} display="inline-block" _hover={{ fontStyle: "italic" }}>
             Back to balances
             <ChevronLeftIcon />
         </Link>
@@ -35,6 +35,7 @@ export default function Send(): ReactElement {
     const { isConnected, address, chain } = useAccount();
     const { connectors, connect } = useConnect();
     const { disconnect } = useDisconnect();
+    const { data: hash, sendTransaction } = useSendTransaction();
 
     // const [bgColor, setBgColor] = useState("gray.400");
     const [bgColor, setBgColor] = useState("red.400");
@@ -45,6 +46,17 @@ export default function Send(): ReactElement {
     const nativeToken = getNativeTokenName(chain?.name);
     const isETH = routerQuery == nativeToken;
     let tokenAddress: `0x${string}` | null;
+
+    function sendTx(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        const formData = new FormData(e.target as HTMLFormElement);
+        const to = formData.get("address") as `0x${string}`;
+        const value = formData.get("amount") as string;
+        if (isETH) {
+            sendTransaction({ to, value: parseEther(value) });
+        } else {
+        }
+    }
 
     if (isETH) {
         return (
@@ -68,49 +80,69 @@ export default function Send(): ReactElement {
                         Send {nativeToken}:
                     </Heading>
                     <Balance address={address} tokenAddress={undefined} isHoverEffectEnabled={false} />
-                    <InputGroup mt={3}>
-                        <Input
-                            width={300}
-                            height={16}
-                            fontSize={36}
-                            placeholder="0.0"
-                            borderColor="white"
-                            borderRadius={0}
-                            borderWidth={4}
-                            _placeholder={{ color: "whiteAlpha.500" }}
-                            _hover={{ borderColor: "white" }}
-                            _active={{ borderColor: "white", boxShadow: "none" }}
-                            _focusVisible={{ borderColor: "white", boxShadow: "none" }}
-                        />
-                        <InputRightAddon bgColor="transparent" border="none" h="auto">
-                            <Text fontSize={40}>{nativeToken}</Text>
-                        </InputRightAddon>
-                    </InputGroup>
-                    <InputGroup size="md" my={3}>
-                        <Input
-                            placeholder="0x..."
-                            borderColor="white"
-                            borderWidth={4}
-                            borderRadius={0}
-                            _placeholder={{ color: "whiteAlpha.500" }}
-                            _hover={{ borderColor: "white" }}
-                            _active={{ borderColor: "white", boxShadow: "none" }}
-                            _focusVisible={{ borderColor: "white", boxShadow: "none" }}
-                        />
-                        <InputRightAddon borderRadius={0} p={0}>
-                            <Button
-                                color="white"
-                                bgColor={bgColor}
-                                borderWidth={4}
-                                borderLeftWidth={0}
-                                borderRadius={0}
+                    <form onSubmit={sendTx}>
+                        <InputGroup mt={3}>
+                            <Input
+                                width={300}
+                                height={16}
+                                fontSize={36}
+                                placeholder="0.0"
                                 borderColor="white"
-                                _hover={{ color: bgColor, bgColor: "white", borderColor: "white" }}
+                                borderRadius={0}
+                                borderWidth={4}
+                                _placeholder={{ color: "whiteAlpha.500" }}
+                                _hover={{ borderColor: "white" }}
+                                _active={{ borderColor: "white", boxShadow: "none" }}
+                                _focusVisible={{ borderColor: "white", boxShadow: "none" }}
+                                name="amount"
+                                required={true}
+                            />
+                            <InputRightAddon bgColor="transparent" border="none" h="auto">
+                                <Text fontSize={40}>{nativeToken}</Text>
+                            </InputRightAddon>
+                        </InputGroup>
+                        <InputGroup size="md" my={3}>
+                            <Input
+                                placeholder="0x..."
+                                borderColor="white"
+                                borderWidth={4}
+                                borderRadius={0}
+                                _placeholder={{ color: "whiteAlpha.500" }}
+                                _hover={{ borderColor: "white" }}
+                                _active={{ borderColor: "white", boxShadow: "none" }}
+                                _focusVisible={{ borderColor: "white", boxShadow: "none" }}
+                                name="address"
+                                required
+                            />
+                            <InputRightAddon borderRadius={0} p={0}>
+                                <Button
+                                    color="white"
+                                    bgColor={bgColor}
+                                    borderWidth={4}
+                                    borderLeftWidth={0}
+                                    borderRadius={0}
+                                    borderColor="white"
+                                    _hover={{ color: bgColor, bgColor: "white", borderColor: "white" }}
+                                    type="submit"
+                                >
+                                    Send
+                                </Button>
+                            </InputRightAddon>
+                        </InputGroup>
+                    </form>
+                    {hash && (
+                        <Text>
+                            Tx Hash:{" "}
+                            <Link
+                                href={chain?.blockExplorers.default.url + "/tx/" + hash}
+                                target="_blank"
+                                textDecoration="underline"
+                                _hover={{ fontStyle: "italic" }}
                             >
-                                Send
-                            </Button>
-                        </InputRightAddon>
-                    </InputGroup>
+                                {hash}
+                            </Link>
+                        </Text>
+                    )}
                     <BackToTopLink />
                 </WalletWrapper>
             </>
